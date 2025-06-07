df6ad6e1c8006935e81c29abccdc4b9760696936
ZAINABKANGSIWALA <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-06-06 22:15:50 -0700

    Time: 4 ms (19.60%) | Memory: 28.3 MB (5.98%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+    unordered_map<int,int> freq;
+    int n=nums.size();
+    for(auto num : nums)
+    {
+        freq[num]++;
+        if(freq[num]>n/2) return num;
+    }
+    return 0;
+    }
+};
