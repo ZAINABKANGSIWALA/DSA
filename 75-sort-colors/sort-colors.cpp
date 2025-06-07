efcce28f89397fce16034684acffff2bb6d32383
ZAINABKANGSIWALA <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-06-06 21:13:50 -0700

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.88%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            int min=i;
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[min]>nums[j]) min=j;
+            }
+            swap(nums[i],nums[min]);
+    }
+    }
+};
