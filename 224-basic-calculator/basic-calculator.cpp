b268c09f0ef98e0bb7aa2d117b7c65d06eb9f269
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-08-18 16:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (58.06%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        int ans = 0;
+        int num = 0;
+        int sign = 1;
+        for(char c : s)
+        {
+            if(isdigit(c))
+            num = num * 10 + (c - '0');
+            else if(c=='+')
+            {
+                ans+=sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if(c == '-')
+            {
+                ans+=sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if(c == '(')
+            {
+                st.push(ans);
+                st.push(sign);
+                ans = 0;
+                sign = 1;
+            }
+            else if(c == ')')
+            {
+                ans +=sign * num;
+                num = 0;
+                int prevSign = st.top(); st.pop();
+                int prev = st.top(); st.pop();
+                ans = prev + prevSign * ans;
+            }
+        }
+        ans += sign * num;
+        return ans;
+    }
+};
