2b17c8db2abc51456f078ee52a2081ad9d0b5e10
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-08-20 23:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (98.70%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     bool wordPattern(string pattern, string s) {
         unordered_map<char,string> mp;
-        unordered_map<string,char> rev;
-        int i =0;
+        unordered_map<string, char> rev;
+        int i=0;
         for(char ch : pattern)
         {
-            string curr = "";
+            string curr="";
             while(i<s.size() && s[i]!=' ') curr+=s[i++];
             if(i<s.size() && s[i]==' ') i++;
             if(curr == "") return false;
             if(mp.find(ch)==mp.end() && rev.find(curr)==rev.end())
             {
-                mp[ch] = curr;
-                rev[curr] = ch;
+                mp[ch]=curr;
+                rev[curr]=ch;
             }
-            else
+            else 
             {
-                 if(mp.find(ch)==mp.end() || mp[ch]!=curr) return false; 
-                if(rev.find(curr)==rev.end() || rev[curr]!=ch) return false; 
-
+                if(mp.find(ch)==mp.end() || mp[ch]!=curr) return false;
+                if(rev.find(curr)==rev.end() || rev[curr]!=ch) return false;
             }
-           
         }
-        if(i < s.size()) return false;
+        if(i<s.size()) return false;
         return true;
     }
 };
