21258e3e4c173c602c457ace85150ef5bca6f54e
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-08-30 10:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (98.15%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = size(nums), i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
-	while(lastJumpedPos < n - 1) {  // loop till last jump hasn't taken us till the end
-		maxReachable = max(maxReachable, i + nums[i]);  // furthest index reachable on the next level from current level
-		if(i == lastJumpedPos) {			  // current level has been iterated & maxReachable position on next level has been finalised
-			lastJumpedPos = maxReachable;     // so just move to that maxReachable position
-			jumps++;                          // and increment the level
-	// NOTE: jump^ only gets updated after we iterate all possible jumps from previous level
-	//       This ensures jumps will only store minimum jump required to reach lastJumpedPos
-		}            
-		i++;
-	}
-	return jumps;
+        int jumps =0, farthest =0, end =0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+         farthest = max(farthest, i+nums[i]);
+         if(i==end)
+         {
+            jumps++;
+            end=farthest;
+         }
+        }
+        return jumps;
     }
 };
