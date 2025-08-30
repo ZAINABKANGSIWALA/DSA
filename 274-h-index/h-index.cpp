2ca43ff44c8187583ca86aaf3c76427f3c7c6f89
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-08-30 21:52:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (26.09%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-      sort(citations.begin(),citations.end(), greater<int>());
-      int h=0;
-      for( int i=0;i<citations.size();i++)
-      {
-        if(citations[i]>=i+1) h++;
-        else break;
-      }
-      return h;
+     int n = citations.size();
+     vector<int> bucket(n+1,0);
+     for(int c : citations)
+     {
+        if(c>=n) bucket[n]++;
+        else bucket[c]++;
+     }
+     int count =0;
+     for(int i = n;i>=0;i--)
+     {
+        count+=bucket[i];
+        if(count>=i) return i;
+     }
+     return 0;
     }
 };
