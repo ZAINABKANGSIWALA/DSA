0db269198d24d6af366aaac9335fc343eb8dc811
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-06 19:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (84.72%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,7 +4,7 @@ public:
         if(strs.empty()) return "";
         for(int i=0;i<strs[0].size();i++)
         {
-            int ch = strs[0][i];
+            char ch = strs[0][i];
             for(int j=1;j<strs.size();j++)
             {
                 if(i>=strs[j].size() || strs[j][i]!=ch)
