f58d3a4486893a29e16a7eabe0ff2e52f9a91f57
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-06 11:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (85.75%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     string intToRoman(int num) {
         string ans;
-string Symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-    for (int i = 0; i < 13; i++)
-    {
-        while (num >= values[i])
+        string Symbols[] ={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int values[]={1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        for(int i=0;i<13;i++)
         {
-            ans += Symbols[i];
-            num -= values[i];
+            while(num >= values[i])
+            {
+                ans += Symbols[i];
+                num -= values[i];
+            }
         }
-    }
-    
-
-    return ans;
+        return ans;
     }
 };
