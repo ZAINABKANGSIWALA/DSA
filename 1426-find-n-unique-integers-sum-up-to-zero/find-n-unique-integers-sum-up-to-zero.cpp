dbacba243423ef8ba748c64759a17a62c7042bc3
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-07 10:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (13.58%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,28 +2,8 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> sum(n);
-        if(n == 1) return {0};
-        if(n % 2 == 0)
-        {
-            int j = 1;
-            for(int i = 0; i < n; i++, j++)
-            {
-                sum[i++] = j;
-                sum[i] = -j;
-            }
-        }
-        else
-        {
-           sum[0] = 1;
-           sum[1] = 2;
-           sum[2] = -3;
-           int j = 4;
-           for(int i = 3; i < n; i++, j++)
-           {
-            sum[i++] = j;
-            sum[i] = -j;
-           }
-        }
+        sum[0] = n*(1-n)/2;
+        for(int i  = 1; i < n; i++)  sum[i] = i;
         return sum;
     }
 };
