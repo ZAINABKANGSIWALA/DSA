232ac86f95afed772bc2e5817869aec66fce970d
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-09 20:55:30 +0530

    Time: 3 ms (60.05%) | Memory: 11.9 MB (50.90%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int> mp;
-        for(char c : magazine) mp[c]++;
-        for(char c : ransomNote)
-        {
-            if(mp[c]==0) return false;
-            mp[c]--;
-        }
-        return true;
+      unordered_map<char,int> mp;
+      for(int c : magazine) mp[c]++;
+      for(int c : ransomNote)
+      {
+        if(mp[c]==0) return false;
+        mp[c]--;
+      }
+      return true;
     }
 };
