1fcad5ff67181f06ccbc932512a04764b54a8022
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-17 01:35:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-       sort(s.begin(),s.end());
-       sort(t.begin(),t.end());
-       for(int i=0;i<s.size();i++)
-       {
-        if(s[i]!=t[i]) return false;
-       }
-       return true;
+      if(s.size()!=t.size()) return false;
+      vector<int> freq(26,0);
+      for(char c : s) freq[c-'a']++;
+      for(char c : t)
+      {
+        if(freq[c-'a']==0) return false;
+        freq[c-'a']--;
+      }
+      return true;
     }
 };
