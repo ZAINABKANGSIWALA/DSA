484a561ba17f38505756aea06ac3bd0d582145d0
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-17 01:09:20 +0530

    Time: 1 ms (69.74%) | Memory: 11.7 MB (74.91%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
     vector<int> freq(26,0);
-    for(int c:magazine) freq[c-'a']++;
-    for(int c:ransomNote)
+    for(char c:magazine) freq[c-'a']++;
+    for(char c:ransomNote)
     {
         if(freq[c-'a']==0) return false;
         freq[c-'a']--;
