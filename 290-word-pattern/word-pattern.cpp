2517bcb13e28b43461c66ee3e68d3fb5127f32ff
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-17 01:31:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (70.32%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
+    stringstream ss(s);
     string word;
     vector<string> words;
-    stringstream ss(s);
     while(ss >> word) words.push_back(word);
     if(pattern.size()!=words.size()) return false;
-    unordered_map<char,string> mp;
-    unordered_map<string,char> rev;
-    for(int i=0;i<pattern.size();i++)
+    unordered_map<char , string> mp;
+    unordered_map<string, char> rev;
+    for(int i=0;i<words.size();i++)
     {
-        char ch=pattern[i];
-        string curr=words[i];
-        if(mp.find(ch)==mp.end() && rev.find(curr)==rev.end())
+        char ch = pattern[i];
+        string curr = words[i];
+        if(mp.find(ch)==mp.end() && rev.find(curr)==rev.end()) 
         {
             mp[ch]=curr;
             rev[curr]=ch;
-        }
-        else 
-        {
-            if(mp[ch]!=curr || rev[curr]!=ch) return false;
-        }
+        } 
+        else if(mp[ch]!=curr || rev[curr]!=ch) return false;
     }
     return true;
-
     }
 };
