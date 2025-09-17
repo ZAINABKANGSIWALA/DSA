e387cd9248a75028c60a14ff237ce6375c879e28
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2025-09-17 13:53:29 +0530

    Time: 129 ms (72.85%) | Memory: 162.6 MB (89.75%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,39 +1,38 @@
 class FoodRatings {
-
-   struct Food
-   {
-    string name;
-    int rating;
-    bool operator<(const Food &other) const
+    //Lets start
+    struct Food
     {
-        if(rating!=other.rating) return rating>other.rating;
-        return name<other.name;
-    }
-   };
-   unordered_map<string,pair<string,int>> foodInfo;
-   unordered_map<string,set<Food>> cuisineFood;
+        string name;
+        int rating;
+        bool operator<(const Food &other) const
+        {
+            if(rating!=other.rating) return rating>other.rating;
+            return name<other.name;
+        }
+    };
+    unordered_map<string,pair<string,int>> foodInfo;
+    unordered_map<string,set<Food>> cuisineFood;
    public:
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        
         for(int i=0;i<foods.size();i++)
         {
-        string food=foods[i];
-        string cuisine=cuisines[i];
-        int rating=ratings[i];
-        foodInfo[food]={cuisine,rating};
-        cuisineFood[cuisine].insert({food,rating});
+            string food=foods[i];
+            string cuisine=cuisines[i];
+            int rating=ratings[i];
+            foodInfo[food]={cuisine,rating};
+            cuisineFood[cuisine].insert({food,rating});
         }
     }
     
     void changeRating(string food, int newRating) {
-        auto [cuisine,oldRating]=foodInfo[food];
-        cuisineFood[cuisine].erase({food,oldRating});
+       auto[cuisine,oldRating]=foodInfo[food];
+       cuisineFood[cuisine].erase({food,oldRating});
         cuisineFood[cuisine].insert({food,newRating});
         foodInfo[food]={cuisine,newRating};
     }
     
     string highestRated(string cuisine) {
-        return cuisineFood[cuisine].begin()->name;
+      return cuisineFood[cuisine].begin()->name;
     }
 };
 
