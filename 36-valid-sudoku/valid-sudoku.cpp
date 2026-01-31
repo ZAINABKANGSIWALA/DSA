eeada6bc04851975165312e04990165505a8fc22
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2026-02-01 01:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (67.07%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bool row[9][9] = {false};
-        bool col[9][9] = {false};
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
         bool boxes[9][9] = {false};
-
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
@@ -12,11 +11,10 @@ public:
                 if(board[i][j]!='.')
                 {
                     int num = board[i][j]-'1';
-                    int boxIndex = (i/3)*3 + (j/3);
-                    if(row[i][num] || col[j][num] || boxes[boxIndex][num]) return false;
-                    row[i][num] = col[j][num] = boxes[boxIndex][num] =  true;
+                    int boxIndex = (i/3) * 3 + (j/3);
+                    if(rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
                 }
-
             }
         }
         return true;
