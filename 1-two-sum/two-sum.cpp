482b7e0a850a604015e604994019cf538ff93b78
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2026-01-31 09:39:37 +0530

    Time: 3 ms (68.06%) | Memory: 14.7 MB (52.37%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            int elem = target-nums[i];
-            if(mp.find(elem)!=mp.end()) return {i,mp[elem]};
-            mp[nums[i]]=i;
-        }
-        return {};
+       unordered_map<int,int> mp;
+       for(int i=0;i<nums.size();i++)
+       {
+        int elem = target - nums[i];
+        if(mp.find(elem)!=mp.end()) return {i,mp[elem]};
+        mp[nums[i]] = i;
+       }
+       return {};
     }
+    /* 1. Initialize an empty array.
+        2. Traverse the array.
+        3. Calculate the complement for the current number.
+        4. If complement exists in map, return indices.
+        5. Else store the value in the map.
+        */
 };
