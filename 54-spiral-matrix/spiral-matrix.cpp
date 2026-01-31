4fa6200073aee6a56552b8923f9263fa355fcf82
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2026-02-01 02:21:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (79.92%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -7,12 +7,12 @@ public:
         int y = 0;
         int dx = 1;
         int dy = 0;
-        vector<int> res;
-        for(int i = 0; i < rows * cols ; i++)
+        vector<int > res;
+        for(int i=0;i<rows*cols;i++)
         {
             res.push_back(matrix[y][x]);
-            matrix[y][x] = -101;
-            if(!(0<=x+dx && x+dx<cols && 0<=y+dy && y+dy<rows) || matrix[y+dy][x+dx]==-101) 
+            matrix[y][x]=-101;
+            if(!(0<=x+dx && x+dx<cols && 0<=y+dy && y+dy<rows)|| matrix[y+dy][x+dx]==-101)
             {
                 int temp = dx;
                 dx = -dy;
@@ -22,6 +22,5 @@ public:
             y+=dy;
         }
         return res;
-
     }
 };
