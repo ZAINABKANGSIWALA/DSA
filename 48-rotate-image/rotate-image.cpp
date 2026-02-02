67416d586695fdc0e4db2a281aaf45bff89f02c6
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2026-02-02 23:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (9.41%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,14 +3,14 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int rows = matrix.size();
         int cols = matrix[0].size();
-        vector<vector<int>> ma(rows, vector<int>(cols,0));
-        for(int i = 0; i < rows; i++)
+        vector<vector<int>> ma(rows,vector<int>(cols,0));
+        for(int i=0;i<rows;i++)
         {
-            for(int j = 0; j < cols; j++)
+            for(int j=0;j<cols;j++)
             {
-                ma[j][cols-1-i] = matrix[i][j];
+                ma[j][cols-1-i]=matrix[i][j];
             }
         }
-        matrix = ma;
+        matrix=ma;
     }
 };
