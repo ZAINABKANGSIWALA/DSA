9f3a43e45867391b244a7c4e65e7258d462d2648
Zainab Kangsiwala <140146124+ZAINABKANGSIWALA@users.noreply.github.com>
2026-02-03 00:36:12 +0530

    Time: 14 ms (18.41%) | Memory: 19.7 MB (7.92%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i =0;
-        for(auto ele : nums)
+        if(nums.size()<=2) return nums.size();
+        int k=2;
+        for(int i=2;i<nums.size();i++)
         {
-            if(i==0 || i==1 || nums[i-2]!=ele) nums[i++] = ele;
-
-        }
-        return i;
+            if(nums[i]!=nums[k-2]) nums[k++]=nums[i];
+        }    
+        return k;
     }
 };
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-  struct ___ { static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; } };
-  std::atexit(&___::_);
-  return 0;
-}();
-#endif
